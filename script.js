85fc3d53bb43f107e5cb41107687992e9bbe4da2
rituparna-sarkar <130404465+rituparna-sarkar@users.noreply.github.com>
2023-12-20 23:26:37 +0530

    Adding files

---
+++ script.js
@@ -0,0 +1,57 @@
+document.getElementById("validation").addEventListener('click', function () {
+    // document.getElementsByTagName('body')[0].style.filter='blur(2px)'
+    document.getElementsByClassName('form')[0].style.display = 'block'
+    return document.getElementById('Email').value="";
+     
+
+    // document.getElementsByClassName('form').style.filter='none'
+}
+
+
+)
+
+
+// document.getElementById("validation").addEventListener('dblclick', function () {
+//     // document.getElementsByTagName('body')[0].style.filter='blur(2px)'
+//     document.getElementsByClassName('form')[0].style.display = 'none'
+
+
+//     // document.getElementsByClassName('form').style.filter='none'
+// }
+
+// )
+
+// document.getElementsByTagName('body')[0].addEventListener('dblclick', function () {
+//     document.getElementById('forvali').style.display = "none";
+// })
+
+
+
+
+// document.getElementById('login').addEventListener('click', function () {
+    
+//     let email, password;
+//     email = document.getElementById('Email').value;
+//     password = document.getElementById('pws').value;
+//     if (email == "") {
+//         document.getElementById('eErr').style.display = "block";
+//     } else {
+//         document.getElementById('eErr').style.display = "none";
+//     }
+
+//     if (password == "") {
+//         document.getElementById('pErr').style.display = "block";
+//     } else {
+//         document.getElementById('pErr').style.display = "none";
+//     }
+
+//     if (email != "" && password != "") {
+//         alert("you are logged in successfully")
+//         document.getElementById("forvali").style.display="none";
+        
+//     }
+   
+
+    
+// }
+// )
